828c12fd3b00d528a91af520411fb942323e7986
Linus Herterich <linus.herterich@gmail.com>
2023-09-10 21:56:01 +0200

    Navigator URL Share: Firefox Bugfix

--- client/src/components/FloorplanIndicator.js
+++ client/src/components/FloorplanIndicator.js
@@ -22,16 +22,26 @@ function FloorplanIndicator() {
     return null;
   }
 
-  const shareData = {
-    title: 'Teile deinen Sitzplatz (' + code + ')',
-    text: 'Ich bin in der Mensa und sitze bei "' + code + '". \n' + window.location.href,
-  };
-
   const shareUrl = () => {
+    const shareData = {
+      title: 'Teile deinen Sitzplatz (' + code + ')',
+      text: 'Ich bin in der Mensa und sitze bei "' + code + '". \n' + window.location.href,
+    };
+
+    const shareDataUrlOnly = { url: window.location.href };
+
     if (navigator.share) {
-      navigator.share(shareData)
-        .then(() => console.log('URL shared successfully'))
-        .catch((err) => console.log('Error: ' + err));
+      if (navigator.canShare && navigator.canShare(shareData)) {
+        navigator.share(shareData)
+          .then(() => console.log('URL shared successfully'))
+          .catch((err) => console.log('Error: ' + err));
+      } else if (navigator.canShare && navigator.canShare(shareDataUrlOnly)) {
+        navigator.share(shareDataUrlOnly)
+          .then(() => console.log('URL shared successfully without text'))
+          .catch((err) => console.log('Error: ' + err));
+      } else {
+        console.log('Share data cannot be shared');
+      }
     } else if (navigator.clipboard) {
       navigator.clipboard.writeText(shareData.text)
         .then(() => {
@@ -46,7 +56,7 @@ function FloorplanIndicator() {
 
   return (
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, borderRadius: '30px 30px 0 0', boxShadow: '0px -10px 20px 0 rgba(0, 0, 0, 0.1)', backgroundColor: theme.palette.primary.main, paddingBottom: 'calc(env(safe-area-inset-bottom) + ' + bottomPadding + 'px)', transition: 'padding 0.5s' }}>
-    {/* // <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, borderRadius: '30px 30px 0 0', boxShadow: '0px -10px 20px 0 rgba(0, 0, 0, 0.1)', backgroundColor: theme.palette.primary.main, paddingBottom: bottomPadding + 'px', transition: 'padding 0.5s' }}> */}
+      {/* // <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, borderRadius: '30px 30px 0 0', boxShadow: '0px -10px 20px 0 rgba(0, 0, 0, 0.1)', backgroundColor: theme.palette.primary.main, paddingBottom: bottomPadding + 'px', transition: 'padding 0.5s' }}> */}
       <Grid container alignItems="center" justifyContent="center" sx={{ mt: 2, mb: 2 }}>
         <Grid item sx={{ mr: 2 }}>
           <Typography variant="h6" color="white" fontWeight="bold" >
