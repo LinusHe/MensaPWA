5ae9d63007d576ba3c9947d88fb4744a0f810557
Linus Herterich <linus.herterich@gmail.com>
2023-09-10 21:44:00 +0200

    Generating menu five days in advance

--- nightly_task/script.py
+++ nightly_task/script.py
@@ -1,53 +1,129 @@
 import os
 import requests
+import logging
+import time
 from datetime import date
+from datetime import timedelta
 import json
 import re  # import the regex module
+import sys
 from image_generator.image_generator import generate_image
 from nutrition_generator.nutrition_generator import generate_chat_completion
 
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
+# Get the current timestamp
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Define the log file path
+log_file_path = os.path.join(script_dir, 'logs', f'{timestamp}.log')
+
+# Create the logs directory if it doesn't exist
+os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+
+# Set up the logger
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s %(message)s')
+
+# Add a StreamHandler to the logger to print messages to the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(message)s')
+console_handler.setFormatter(formatter)
+logging.getLogger().addHandler(console_handler)
+
 # Define output directory
 relativeOutputDir = "./out"
 output_dir = os.path.join(script_dir, relativeOutputDir)
 
-# Define URL and headers
-url = f"https://app.htwk-leipzig.de/api/canteens/01FG1RPGG52ZKR7QABF75DCEP4/menus/{date.today().isoformat()}?page=1&itemsPerPage=30"
-headers = {'accept': 'application/json'}
+# Get today's date
+today = date.today()
+# Loop over the next five days
+for i in range(5):
+    # Get the date for the current iteration
+    current_date = (today + timedelta(days=i)).isoformat()
+    logging.info(f"Processing data for date: {current_date}")
 
-# Request data from the API
-response = requests.get(url, headers=headers)
+    # Define URL for the current date
+    url = f"https://app.htwk-leipzig.de/api/canteens/01FG1RPGG52ZKR7QABF75DCEP4/menus/{current_date}?page=1&itemsPerPage=30"
+    headers = {'accept': 'application/json'}
 
-# If the request was successful
-if response.status_code == 200:
-    data = response.json()
-    
-    # Get current date as a string
-    current_date = date.today().isoformat()
+    # Request data from the API
+    response = requests.get(url, headers=headers)
 
-    # Make directory for current date if it doesn't exist
-    os.makedirs(f'{output_dir}/{current_date}', exist_ok=True)
+    # If the request was successful
+    if response.status_code == 200:
+        new_data = response.json()
+        # Check if data is empty or an empty array
+        if not new_data:
+            logging.info("No menu for " + current_date + ".")
+            #skip this iteration
+            continue
 
-    # Save the received data into a json file
-    with open(f'{output_dir}/{current_date}/menu.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        # Define the path for the current date's directory
+        current_dir = f'{output_dir}/{current_date}'
 
-    # Iterate over all food items in the data
-    for item in data:
-        # Extract the item title
-        title = item["title"]
+        # Create the directory if it doesn't exist
+        os.makedirs(current_dir, exist_ok=True)
 
-        # Remove any non-alphanumeric characters and replace with underscore
-        safe_title = re.sub(r'\W+', '_', title)
+        # Define the path for the menu.json file
+        menu_file_path = f'{current_dir}/menu.json'
 
-        # Call the function to generate chat completion and save the response
-        generate_chat_completion(title, current_date, safe_title, script_dir, output_dir)
-        
-        # Call the function to generate image and save it
-        try:
-            generate_image(title, current_date, safe_title, script_dir, output_dir)
-        except Exception as e:
-            # skip image, if there is an Error
-            print(f"Skipping item '{title}' due to error: {str(e)}")
+        # Check if the directory already exists
+        if os.path.exists(current_dir):
+            # Check if the menu.json file already exists
+            if os.path.exists(menu_file_path):
+                # Load the existing data
+                with open(menu_file_path, 'r', encoding='utf-8') as f:
+                    existing_data = json.load(f)
+
+                # Extract the titles from the existing and new data
+                existing_titles = [item['title'] for item in existing_data]
+                new_titles = [item['title'] for item in new_data]
+
+                # Compare the food titles in the existing data with the new data
+                if set(existing_titles) == set(new_titles):
+                    # If the titles are the same, skip this iteration
+                    logging.info("Food titles are the same, skipping iteration.")
+                    continue
+
+                # If the titles are different, delete the existing menu.json file and the generated images
+                logging.info("Food titles are different, deleting existing menu.json file and generated images.")
+                os.remove(menu_file_path)
+                for file in os.listdir(current_dir):
+                    if file.endswith('.jpg'):
+                        os.remove(os.path.join(current_dir, file))
+
+        # Save the new data into a json file
+        with open(menu_file_path, 'w', encoding='utf-8') as f:
+            json.dump(new_data, f, ensure_ascii=False, indent=4)
+
+        # Iterate over all food items in the new data
+        for item in new_data:
+            # Extract the item title
+            title = item["title"]
+
+            # Remove any non-alphanumeric characters and replace with underscore
+            safe_title = re.sub(r'\W+', '_', title)
+
+            # Call the function to generate chat completion and save the response
+            try:
+                logging.info(f"Generating chat completion for item: {title}")
+                generate_chat_completion(title, current_date, safe_title, script_dir, output_dir)
+            except Exception as e:
+                # skip chat completion, if there is an Error
+                logging.error(f"Skipping item '{title}' due to error: {str(e)}")
+            
+            # Call the function to generate image and save it
+            try:
+                logging.info(f"Generating image for item: {title}")
+                generate_image(title, current_date, safe_title, script_dir, output_dir)
+            except Exception as e:
+                # skip image, if there is an Error
+                logging.error(f"Skipping item '{title}' due to error: {str(e)}")
+
+    else:
+        logging.error(f"Error: Request failed with status code {response.status_code}")
