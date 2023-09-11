8cf3dce847ac475a71aaca94532a75b3d7f8f4b3
Linus Herterich <linus.herterich@gmail.com>
2023-09-11 14:00:12 +0200

    fix: mapping nginx data with public-data volume

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -61,6 +61,19 @@ registerRoute(
   })
 );
 
+registerRoute(
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.origin === self.location.origin && url.pathname.startsWith('/data'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  new StaleWhileRevalidate({
+    cacheName: 'data',
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used data are removed.
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
